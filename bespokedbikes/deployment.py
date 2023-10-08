efbd6010b26c8b3f218ec287c201d88c2206371f
Thuan Vo <thuanvo09@hotmail.com>
2023-10-08 04:06:45 -0400

    test

--- bespokedbikes/deployment.py
+++ bespokedbikes/deployment.py
@@ -4,7 +4,8 @@ from .settings import BASE_DIR
 
 
 
-SECRET_KEY = os.environ['SECRET']
+SECRET_KEY = os.environ.get('SECRET')
+print(f"SECRET_KEY: {SECRET_KEY}")
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME'], 'bespoked-bikes.azurewebsites.net', 'www.bespoked-bikes.azurewebsites.net', '127.0.0.1', '000.00.00.00']
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
