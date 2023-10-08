176e89f95036e4b2397bf158218d1cb19da490b9
Thuan Vo <thuanvo09@hotmail.com>
2023-10-08 06:55:09 -0400

    trust host

--- bespokedbikes/settings.py
+++ bespokedbikes/settings.py
@@ -19,12 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-print("run normal")
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'dab28b3eb7a77'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug taurned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['bespoked-bikes.azurewebsites.net', 
@@ -95,7 +95,7 @@ DATABASES = {
         'HOST': os.environ.get('DBHOST'),
         'USER': os.environ.get('DBUSER'),
         'PASSWORD': os.environ.get('DBPASS'),
-        'PORT': '5432',
+        'PORT': '5433',
         'OPTIONS': {'sslmode': 'require'},
     }
 }
