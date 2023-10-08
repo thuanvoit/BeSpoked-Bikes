9672ccdbf133ccdafe591c4bc8619114e5f37425
Thuan Vo <thuanvo09@hotmail.com>
2023-10-08 02:21:26 -0400

    deploy config

--- bespokedbikes/wsgi.py
+++ bespokedbikes/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+settings_module = 'bespokedbikes.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'bespokedbikes.settings'
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bespokedbikes.settings')
 
 application = get_wsgi_application()
