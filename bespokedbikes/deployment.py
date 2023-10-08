47af40f5371ae89774917c24e3097dd442343458
Thuan Vo <thuanvo09@hotmail.com>
2023-10-08 02:57:35 -0400

    secret

--- bespokedbikes/deployment.py
+++ bespokedbikes/deployment.py
@@ -3,6 +3,7 @@ from .settings import *
 from .settings import BASE_DIR
 
 
+
 SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
